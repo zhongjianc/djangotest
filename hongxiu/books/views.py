64f19dbbaf09663a6cb59bdef070a47333ee2cb7
unknown <zjc@ele.com>
2018-06-21 16:07:28 +0800

    rrr

--- hongxiu/books/views.py
+++ hongxiu/books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from django.template.loader import get_template
 from django.shortcuts import render_to_response
+from books.models import Book
 import datetime
 # Create your views here.
 
@@ -18,9 +19,19 @@ def test(request):
 def search_form(request):
     return render_to_response('search_form.html')
 
+'''
 def search(request):
     if 'q' in request.GET:
         message = 'You searched for: %r' % request.GET['q']
     else:
         message = 'You submitted an empty form.'
-    return HttpResponse(message)
+    return HttpResponse(message)'''
+
+def search(request):
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        #books = Book.objects.filter(title__icontains=q)
+        return render_to_response('search_results.html',
+            {'books': books, 'query': q})
+    else:
+        return HttpResponse('Please submit a search term.')
